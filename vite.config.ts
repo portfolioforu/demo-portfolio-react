b375e579754a70676212a370879bd9f9123c1f4e
portfolioforu <hello.portfolioforu@outlook.com>
2026-09-18 21:30:24 +0200

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/demo-portfolio-react/',
-    plugins: [react(), tailwindcss()],
+      base: '/',
+      plugins: [react(), tailwindcss()],
 })
